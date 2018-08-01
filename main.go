525e66df03db493892c00b52adbca6115993171d
himnynameisriz <v.rizwanali@gmail.com>
2018-08-01 00:22:01 -0500

    Removing unnecessary vars

--- main.go
+++ main.go
@@ -15,14 +15,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-var (
-	Token     string
-	LastTitle string
-	Count     int
-	ChannelId string
-	RssFeed   string
-	LogFile   *os.File
-)
+var LogFile *os.File
 
 var botId string
 
@@ -95,26 +88,26 @@ func main() {
 
 func runRssFeed(s *discordgo.Session, feed Feed) {
 	fmt.Println("RSS Feed reader starting")
-	for ok := true; ok; ok = (Count < 5) {
-		message, lastTitle, err := rssFeed.RunFeed(feed.FeedURL, "")
+	for ok := true; ok; ok = false {
+		message, _, err := rssFeed.RunFeed(feed.FeedURL, "")
 
 		if err != nil {
-			Count++
 			log.Error("Error, ", err)
 		} else {
 			if len(message) == 0 {
 				log.Info("No message found, sleep time")
 			} else {
-				LastTitle = lastTitle
 				log.Info("New message, ", message)
 				log.Info("Attempting to send message")
+
 				s.ChannelMessageSend(feed.ChannelId, message)
+
 				log.Info("Message sent")
 			}
 		}
 
 		log.Info("Sleep has began")
-		time.Sleep(10 * time.Second)
+		time.Sleep(10 * time.Second) // Change this to minute
 		log.Info("Sleep ended")
 	}
 }
